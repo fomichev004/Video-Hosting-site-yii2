5dca92d040481c3b1433a30295f58bc974f6714c
fomichev004 <fomichev004@gmail.com>
2019-08-26 18:42:33 +0300

    Created default controller, veiew and main layout.

--- web/index.php
+++ web/index.php
@@ -1,4 +1,7 @@
 <?php
+
+    define ('YII_DEBUG', true); // дебаг ошибок
+
     require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
     $config = require __DIR__ . '/../config/web.php';
     $yii = new yii\web\Application($config);
